bfe940f655e26e9a5e6c023925046d2b97a872ee
Publio Tapia <pctapia@gmail.com>
2025-05-19 01:38:31 +0200

    add text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I was changed again twice!
-        </p>
+        <p>I was changed in the feature branch!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
